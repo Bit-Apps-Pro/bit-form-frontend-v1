80fca6f861cdac1eb008fb9cd23463c8f149a3b5
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-02-13 17:26:24 +0600

    feat: isloading props in confirm modal

--- src/components/Utilities/ConfirmModal.jsx
+++ src/components/Utilities/ConfirmModal.jsx
@@ -1,7 +1,8 @@
 import { __ } from '../../Utils/i18nwrap'
 import Modal from './Modal'
+import LoaderSm from '../Loaders/LoaderSm'
 
-function ConfirmModal({ close, action, mainMdlCls, show, btnTxt, body, btn2Txt, btn2Action, btnClass, title, className, children, warning }) {
+function ConfirmModal({ close, action, mainMdlCls, show, btnTxt, body, btn2Txt, btn2Action, btnClass, title, className, children, warning, isLoading = false }) {
   return (
     <Modal
       sm
@@ -19,7 +20,10 @@ function ConfirmModal({ close, action, mainMdlCls, show, btnTxt, body, btn2Txt, 
         <div className={`txt-center ${warning && 'mt-3'}`}>
           {!btn2Txt && <button onClick={close} className={`btn btcd-btn-o-gray green w-4 mr-2 br-50 ${!className && 'btn-lg'}`} type="button">{__('Cancel', 'bitform')}</button>}
           {btn2Txt && <button onClick={btn2Action} className={`btn green w-4 mr-2 br-50 ${!className && 'btn-lg'}`} type="button">{btn2Txt}</button>}
-          <button onClick={action} className={`btn ${btnClass || 'red'} w-4 br-50 ${!className && 'btn-lg'}`} type="button">{btnTxt}</button>
+          <button onClick={action} className={`btn ${btnClass || 'red'} w-4 br-50 ${!className && 'btn-lg'}`} type="button">
+            {btnTxt}
+            {isLoading && <LoaderSm size={17} clr="#fff" className="ml-2" />}
+          </button>
         </div>
       </div>
     </Modal>
